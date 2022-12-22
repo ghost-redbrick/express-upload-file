08172bd10477127f398ff97404bf5626cf64c047
Ghost <ghost@redbrick.space>
2022-12-22 04:20:22 +0000

    Apply write a file from bytebuffer

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 import express from "express";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
 
+app.use(express.raw({ limit: "300mb", type: ["*/*"] }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/upload", (req, res) => {
+  console.error(req.file, req.body);
+  // fs.writeFileSync("storage/test.jpg", req.body);
+  console.error("start writing binary to a file");
+  fs.writeFile(
+    "storage/test.zip",
+    req.body,
+    {
+      encoding: "utf8",
+      flag: "w",
+      mode: 0o666,
+    },
+    (err) => {
+      if (err) console.error(err);
+      else {
+        console.error("File written successfully");
+      }
+    }
+  );
+  console.error("end call");
+  res.send("end call");
 });
 
 app.listen(port, () => {
